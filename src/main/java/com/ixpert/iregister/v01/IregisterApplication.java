951851c9cd52c9893a57b81af29a2f3369e2c659
iXpert <amr.elnagar@outlook.com>
2019-11-26 23:35:33 +0100

    commit1_26112019

---
+++ src/main/java/com/ixpert/iregister/v01/IregisterApplication.java
@@ -0,0 +1,13 @@
+package com.ixpert.iregister.v01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IregisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IregisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ixpert/iregister/v01/IregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ixpert.iregister.v01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IregisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
